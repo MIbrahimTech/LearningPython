af108c0677a2b45a1b28334d86ed8239f6fcc4bd
Mohammad Ibrahim Khan <123298616+MIbrahimTech@users.noreply.github.com>
2023-01-22 17:30:02 +0400

    How to use input and string

---
+++ input.py
@@ -0,0 +1,9 @@
+#How to use input and selection statement 
+n1= int(input('Enter a number:  '))
+n2= int(input('Enter another number:  '))
+
+if n1>n2:
+    print(str(n1) +" is greater than "+ str(n2))
+else:
+    print(str(n2) +" is greater than "+ str(n1))
+
---
+++ letter.py
@@ -0,0 +1,7 @@
+#Input and string
+p1=str(input('Enter your name:  '))
+p2=str(input('Enter your fathers name:  '))
+if len (p1)> len (p2):
+    print(p1 +" has more letters than "+ p2)
+else:
+    print(p2 +" Has more letter than "+ p1)
